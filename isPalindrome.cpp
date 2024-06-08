ff6a14f36c2f5a2d270ccc31274b695d0b5692af
Abhishek Ekka <75915784+abhi0ekka@users.noreply.github.com>
2024-06-09 00:30:13 +0530

    Done!!

---
+++ isPalindrome.cpp
@@ -0,0 +1,34 @@
+//
+// Created by ABHISHEK EKKA on 09/06/24.
+//
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int);
+
+int main()
+{
+    int num;
+    cout<<"Enter a number to check weather it is a Palindrome or not"<<endl;
+    cin>>num;
+    if(isPalindrome(num))
+        cout<<"true";
+    else
+        cout<<"false";
+}
+
+bool isPalindrome(int x)
+{
+    int val,rem,pal=0;
+    val=x;
+    if(x<0)
+        return 0;
+    while(x>0)
+    {
+        rem=x%10;
+        pal=pal*10+rem;
+        x=x/10;
+    }
+
+    return val==pal;
+}
